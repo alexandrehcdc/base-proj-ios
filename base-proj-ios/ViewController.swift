f622fc81d18291a45b2535a40e201aeaf9dd0e9e
Alexandre Henrique <alexandrehcdc@gmail.com>
2018-02-02 10:55:34 -0300

    Initial Commit

---
+++ base-proj-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  base-proj-ios
+//
+//  Created by Alexandre Henrique on 02/02/18.
+//  Copyright © 2018 Alexandre Henrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
